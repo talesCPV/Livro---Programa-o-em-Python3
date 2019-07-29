39cf473593bfb835f3e3d7273de4f364ac11e55f
tales <tales@flexibus.com.br>
2019-07-29 15:48:36 -0300

    first

---
+++ exercicio_1_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+lista = []
+while True:
+	try:
+		val = input('Digite um valor ou "Enter" pra finalizar: ')
+		dg = int(val)
+		lista.append(dg)
+	except:
+		if val == '':
+			print('fim')
+			break
+		else:
+			print('Valor digitado não corresponde a um número inteiro.')
+
+
+if len(lista) > 0:
+	i = 0
+	soma = 0
+	minimo = lista[0]
+	maximo = 0
+	while i < len(lista):
+		soma += lista[i]
+		if 
+		i += 1
+
+	print('números: ',lista)
+
+
+
+
+
+
