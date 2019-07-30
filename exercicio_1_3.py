c7d271477fb8fe36ea21bf8e371ced12e0c64ebf
tales <tales@flexibus.com.br>
2019-07-30 10:17:12 -0300

    first

--- exercicio_1_3.py
+++ exercicio_1_3.py
@@ -2,14 +2,32 @@
 
 import random
 
-artigos = ['o','a','os','as','um','uma','algum','nenhum','alguma','nenhuma']
-sujeito = ['gato','cachorro','homem','mulher','cabra','galinha','sapo','menino','menina','computador']
-verbo   = ['atropelou','comeu','deixou','passou','programou','arrumou']
+artigos_m = ['o','um','algum','nenhum']
+artigos_f = ['a','uma','alguma','nenhuma']
+sujeito_m = ['gato','cachorro','homem','bode','galo','sapo','menino','computador']
+sujeito_f = ['gata','cadela','mulher','cabra','galinha','cobra','menina','cadeira']
+verbo     = ['atropelou','comeu','deixou','passou','programou','arrumou','comprou']
 
-frase  = random.choice(artigos) + ' ' 
-frase += random.choice(sujeito) + ' '
-frase += random.choice(verbo) + ' '
-frase += random.choice(artigos) + ' '
-frase += random.choice(sujeito)
+i = 0
+while i < 6:
+	genero = random.randint(0,1)
 
-print(frase)
+	if genero:
+		frase  = random.choice(artigos_f) + ' ' 
+		frase += random.choice(sujeito_f) + ' '
+	else:
+		frase  = random.choice(artigos_m) + ' ' 
+		frase += random.choice(sujeito_m) + ' '
+
+	frase += random.choice(verbo) + ' '
+	genero = random.randint(0,1)
+
+	if genero:
+		frase += random.choice(artigos_f) + ' ' 
+		frase += random.choice(sujeito_f) + ' '
+	else:
+		frase  = random.choice(artigos_m) + ' ' 
+		frase += random.choice(sujeito_m) + ' '
+
+	print(frase)
+	i += 1
