f86d9117dbfb42b529b1b01785029b8ebf063596
tales <tales@flexibus.com.br>
2019-08-06 10:17:38 -0300

    upload

--- upload.py
+++ upload.py
@@ -8,21 +8,15 @@ import os,sys
 # remote add origin https://github.com/talesCPV/Livro---Programa-o-em-Python3.git
 # git push -u origin master
 
-#prg = sys.argv[1]
-#rep = sys.argv[2]
-
-rep = 'python3'
-#prg = []
-#for x in os.listdir('.'):
-#	if x.endswith(".py"):
-#		prg.append(x)
-
+if len(sys.argv) > 1:
+ rep = sys.argv[1]
+else:
+	rep = 'python3'
 
 os.system('git init')
 for x in os.listdir('.'):
 	if x.endswith(".py"):
 		os.system('git add ' + x)
-#os.system('git add ' + prg)
 os.system('git commit -m' + rep)
 os.system('remote add origin https://github.com/talesCPV/Livro---Programa-o-em-Python3.git')
 os.system('git push -u origin master')
